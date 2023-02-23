3e50a687cba29404a6d8c84dea295d3c0c083ea4
Felix Neutatz <neutatz@googlemail.com>
2023-02-23 03:40:13 +0100

    run  full parallel

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_static_ensemble/tmux_parallel_all.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_static_ensemble/tmux_parallel_all.py
@@ -12,7 +12,7 @@ outputname = 'static_ensemble'
 
 conda_name = 'GreenAutoMLD'
 
-parallelism = 1#20#15#multiprocessing.cpu_count()
+parallelism = 20#20#15#multiprocessing.cpu_count()
 server = libtmux.Server()
 
 data_id = 0
