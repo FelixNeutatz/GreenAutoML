9415cd5d74942af2c08b18640113b7bfe7dc5e6f
Felix Neutatz <neutatz@googlemail.com>
2023-02-25 07:25:30 +0100

    run static ensemble

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_static_ensemble/check_model_parallel_per_data_minimum_all1.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_all_only_static_ensemble/check_model_parallel_per_data_minimum_all1.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
                                               main_memory_budget_gb=memory_budget,
                                               differential_privacy_epsilon=privacy,
                                               hold_out_fraction=0.33,
-                                              max_ensemble_models=1,
+                                              max_ensemble_models=50,
                                               )
 
                     best_result = search_default.fit(X_train_hold, y_train_hold, categorical_indicator=categorical_indicator_hold, scorer=my_scorer)
