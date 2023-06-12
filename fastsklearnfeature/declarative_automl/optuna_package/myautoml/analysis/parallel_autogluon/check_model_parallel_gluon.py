74b3d308adb0cb51ee3c099e8c1c9943c2e0b095
Felix Neutatz <neutatz@googlemail.com>
2023-06-12 15:25:53 +0200

    run 003

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_autogluon/check_model_parallel_gluon.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_autogluon/check_model_parallel_gluon.py
@@ -70,10 +70,10 @@ for test_holdout_dataset_id in [args.dataset]:
                 #df_test[label] = y_test_hold
                 my_data_test = TabularDataset(data=df_test)
 
-                presets = ['good_quality_faster_inference_only_refit', 'optimize_for_deployment']
-                #presets = 'best_quality'
+                #presets = ['good_quality_faster_inference_only_refit', 'optimize_for_deployment']
+                presets = 'best_quality'
 
-                predictor = TabularPredictor(label=label, eval_metric='balanced_accuracy', path=tmp_path).fit(train_data=my_data_train, time_limit=search_time_frozen, presets=presets, num_cpus=1)
+                predictor = TabularPredictor(label=label, eval_metric='balanced_accuracy', path=tmp_path).fit(train_data=my_data_train, time_limit=search_time_frozen, presets=presets, num_cpus=1, infer_limit=0.003, infer_limit_batch_size=1)
                 tracker.stop()
 
                 tracker_inference = EmissionsTracker(save_to_file=False)
