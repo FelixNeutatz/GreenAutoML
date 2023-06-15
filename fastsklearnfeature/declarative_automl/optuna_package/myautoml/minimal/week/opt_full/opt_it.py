232f1f82ed1b3962095ecc3947b82faef2ae3e8b
Felix Neutatz <neutatz@googlemail.com>
2023-06-15 12:22:36 +0200

    report difference mean and fix some stuff

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/opt_full/opt_it.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/opt_full/opt_it.py
@@ -6,26 +6,13 @@ import numpy as np
 from fastsklearnfeature.declarative_automl.optuna_package.myautoml.my_system.Space_GenerationTreeBalance import SpaceGenerator
 import copy
 from anytree import RenderTree
-from sklearn.ensemble import RandomForestRegressor
-from optuna.samplers import RandomSampler
 import pickle
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.feature_transformation.FeatureTransformations import FeatureTransformations
 from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import get_data
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import data2features
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import space2features
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import get_feature_names
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import ifNull
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import generate_parameters_minimal_sample_constraints_all_emissions
-from optuna.samplers import TPESampler
 import multiprocessing as mp
-from multiprocessing import Lock
 import openml
 from sklearn.metrics import balanced_accuracy_score
 import multiprocessing
-import sklearn
-from fastsklearnfeature.declarative_automl.fair_data.test import get_X_y_id
 import traceback
-from fastsklearnfeature.declarative_automl.optuna_package.myautoml.utils_model_mine import optimize_accuracy_under_minimal_sample_ensemble
 import getpass
 
 class NoDaemonProcess(multiprocessing.Process):
@@ -51,6 +38,7 @@ class NestablePool(multiprocessing.pool.Pool):
 openml.config.apikey = '4384bd56dad8c3d2c0f6630c52ef5567'
 openml.config.cache_directory = '/home/neutatz/phd2/cache_openml'
 
+my_openml_tasks = [75126, 75125, 75121, 75120, 75116, 75115, 75114, 189859, 189878, 189786, 167204, 190156, 75156, 166996, 190157, 190158, 168791, 146597, 167203, 167085, 190154, 75098, 190159, 75169, 126030, 146594, 211723, 189864, 189863, 189858, 75236, 190155, 211720, 167202, 75108, 146679, 146592, 166866, 167205, 2356, 75225, 146576, 166970, 258, 75154, 146574, 275, 273, 75221, 75180, 166944, 166951, 189828, 3049, 75139, 167100, 75232, 126031, 189899, 75146, 288, 146600, 166953, 232, 75133, 75092, 75129, 211722, 75100, 2120, 189844, 271, 75217, 146601, 75212, 75153, 75109, 189870, 75179, 146596, 75215, 189840, 3044, 168785, 189779, 75136, 75199, 75235, 189841, 189845, 189869, 254, 166875, 75093, 75159, 146583, 75233, 75089, 167086, 167087, 166905, 167088, 167089, 167097, 167106, 189875, 167090, 211724, 75234, 75187, 2125, 75184, 166897, 2123, 75174, 75196, 189829, 262, 236, 75178, 75219, 75185, 126021, 211721, 3047, 75147, 189900, 75118, 146602, 166906, 189836, 189843, 75112, 75195, 167101, 167094, 75149, 340, 166950, 260, 146593, 75142, 75161, 166859, 166915, 279, 245, 167096, 253, 146578, 267, 2121, 75141, 336, 166913, 75176, 256, 75166, 2119, 75171, 75143, 75134, 166872, 166932, 146603, 126028, 3055, 75148, 75223, 3054, 167103, 75173, 166882, 3048, 3053, 2122, 75163, 167105, 75131, 126024, 75192, 75213, 146575, 166931, 166957, 166956, 75250, 146577, 146586, 166959, 75210, 241, 166958, 189902, 75237, 189846, 75157, 189893, 189890, 189887, 189884, 189883, 189882, 189881, 189880, 167099, 189894]
 
 
 search_time = 60#60*5
@@ -60,8 +48,6 @@ repetitions_count = 10#10
 #search_time = 10
 #topk = 3
 #repetitions_count = 1
-
-my_openml_tasks = [75126, 75125, 75121, 75120, 75116, 75115, 75114, 189859, 189878, 189786, 167204, 190156, 75156, 166996, 190157, 190158, 168791, 146597, 167203, 167085, 190154, 75098, 190159, 75169, 126030, 146594, 211723, 189864, 189863, 189858, 75236, 190155, 211720, 167202, 75108, 146679, 146592, 166866, 167205, 2356, 75225, 146576, 166970, 258, 75154, 146574, 275, 273, 75221, 75180, 166944, 166951, 189828, 3049, 75139, 167100, 75232, 126031, 189899, 75146, 288, 146600, 166953, 232, 75133, 75092, 75129, 211722, 75100, 2120, 189844, 271, 75217, 146601, 75212, 75153, 75109, 189870, 75179, 146596, 75215, 189840, 3044, 168785, 189779, 75136, 75199, 75235, 189841, 189845, 189869, 254, 166875, 75093, 75159, 146583, 75233, 75089, 167086, 167087, 166905, 167088, 167089, 167097, 167106, 189875, 167090, 211724, 75234, 75187, 2125, 75184, 166897, 2123, 75174, 75196, 189829, 262, 236, 75178, 75219, 75185, 126021, 211721, 3047, 75147, 189900, 75118, 146602, 166906, 189836, 189843, 75112, 75195, 167101, 167094, 75149, 340, 166950, 260, 146593, 75142, 75161, 166859, 166915, 279, 245, 167096, 253, 146578, 267, 2121, 75141, 336, 166913, 75176, 256, 75166, 2119, 75171, 75143, 75134, 166872, 166932, 146603, 126028, 3055, 75148, 75223, 3054, 167103, 75173, 166882, 3048, 3053, 2122, 75163, 167105, 75131, 126024, 75192, 75213, 146575, 166931, 166957, 166956, 75250, 146577, 146586, 166959, 75210, 241, 166958, 189902, 75237, 189846, 75157, 189893, 189890, 189887, 189884, 189883, 189882, 189881, 189880, 167099, 189894]
 #my_openml_tasks = my_openml_tasks[:10]
 
 mgr = mp.Manager()
@@ -149,7 +135,8 @@ def run_AutoML(task_id):
         shuffle_validation = trial.params['shuffle_validation']
         train_best_with_full_data = trial.params['train_best_with_full_data']
 
-        validation_sampling = trial.params['validation_sampling']
+        if trial.params['use_validation_sampling']:
+            validation_sampling = trial.params['validation_sampling']
 
     for pre, _, node in RenderTree(space.parameter_tree):
         if node.status == True:
@@ -203,9 +190,6 @@ def run_AutoML(task_id):
 
     dynamic_values_I_found = dictionary_felix[task_id]
 
-    if dynamic_values_I_found == 0.0:
-        return np.NAN
-
     dynamic_params = []
     for random_i in range(repetitions_count):
         search = MyAutoML(cv=cv,
@@ -241,14 +225,21 @@ def run_AutoML(task_id):
             traceback.print_exc()
         dynamic_params.append(test_score)
 
-    return np.mean(dynamic_params) / dynamic_values_I_found
+    current_mean = np.mean(dynamic_params)
+
+    if max(current_mean, dynamic_values_I_found) == 0.0 or (current_mean - dynamic_values_I_found) == 0.0:
+        return 0.0
+
+    return (current_mean - dynamic_values_I_found) / max(current_mean, dynamic_values_I_found)
 
 def sample_configuration(trial):
     results = [0.0]
     try:
         gen = SpaceGenerator()
         space = gen.generate_params()
-        space.sample_parameters(trial) # no tuning of the space
+
+        if trial.suggest_categorical('tune_space', [True, False]):
+            space.sample_parameters(trial) # no tuning of the space
 
         trial.set_user_attr('space', copy.deepcopy(space))
 
@@ -294,10 +285,11 @@ def sample_configuration(trial):
             shuffle_validation = trial.suggest_categorical('shuffle_validation', [False, True])
             train_best_with_full_data = trial.suggest_categorical('train_best_with_full_data', [False, True])
 
-            if trial.suggest_categorical('tune_validation_sampling', [True, False]):
-                validation_sampling = trial.suggest_int('validation_sampling', 10, 100000, log=True)
-            else:
-                validation_sampling = trial.suggest_int('validation_sampling', 1000, 1000, log=True)
+            if trial.suggest_categorical('use_validation_sampling', [True, False]):
+                if trial.suggest_categorical('tune_validation_sampling', [True, False]):
+                    validation_sampling = trial.suggest_int('validation_sampling', 10, 100000, log=True)
+                else:
+                    validation_sampling = trial.suggest_int('validation_sampling', 1000, 1000, log=True)
 
         my_random_seed = int(time.time())
 
@@ -321,6 +313,7 @@ def sample_configuration(trial):
         my_dict['space'] = trial.study.best_trial.user_attrs['space']
         my_dict['params'] = trial.study.best_params
         my_dict['value'] = trial.study.best_value
+        my_dict['study'] = trial.study
 
         with open('/home/' + getpass.getuser() + '/data/my_temp/best_params.p', "wb+") as pickle_model_file:
             pickle.dump(my_dict, pickle_model_file)
