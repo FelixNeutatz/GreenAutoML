401fad9cb44a8f9d4da35d489a3c4f7d7d750c59
Felix Neutatz <neutatz@googlemail.com>
2023-06-23 00:47:58 +0200

    run ensemble opt

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/opt_full/opt_it_successive.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/opt_full/opt_it_successive.py
@@ -270,7 +270,8 @@ def sample_configuration(trial):
     if trial.suggest_categorical('use_sampling', [True, False]):
         sample_fraction = trial.suggest_int('sample_fraction', 10, 1000000, log=True)
 
-    use_ensemble = trial.suggest_categorical('use_ensemble', [True, False])
+    #use_ensemble = trial.suggest_categorical('use_ensemble', [True, False])
+    use_ensemble = trial.suggest_categorical('use_ensemble', [True])
 
     if use_ensemble:
         if trial.suggest_categorical('tune_ensemble_size', [True, False]):
