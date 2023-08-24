5234e6170a041dc55366eedf06f1561422039636
Felix Neutatz <neutatz@googlemail.com>
2023-08-24 17:21:19 +0200

    swicth to main

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_tpot_new/check_model_parallel_tpot.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/analysis/parallel_tpot_new/check_model_parallel_tpot.py
@@ -14,8 +14,6 @@ from tpot import TPOTClassifier
 import sklearn
 import multiprocessing
 
-multiprocessing.set_start_method('forkserver')
-
 
 class ConstraintRun(object):
     def __init__(self, space, best_trial, test_score, more=None, tracker=None, tracker_inference=None, len_pred=None):
@@ -113,75 +111,77 @@ parser.add_argument("--outputname", "-o", help="Name of the output file")
 args = parser.parse_args()
 print(args.dataset)
 
+if __name__ == '__main__':
+    multiprocessing.set_start_method('forkserver')
 
-print(args)
+    print(args)
 
-#args.dataset = 168794
+    #args.dataset = 168794
 
-memory_budget = 500.0
-privacy = None
+    memory_budget = 500.0
+    privacy = None
 
-for test_holdout_dataset_id in [args.dataset]:
+    for test_holdout_dataset_id in [args.dataset]:
 
-    X_train_hold, X_test_hold, y_train_hold, y_test_hold, categorical_indicator_hold, attribute_names_hold = get_data('data', randomstate=42, task_id=test_holdout_dataset_id)
+        X_train_hold, X_test_hold, y_train_hold, y_test_hold, categorical_indicator_hold, attribute_names_hold = get_data('data', randomstate=42, task_id=test_holdout_dataset_id)
 
-    dynamic_approach = []
+        dynamic_approach = []
 
-    new_constraint_evaluation_dynamic_all = []
+        new_constraint_evaluation_dynamic_all = []
 
-    for minutes_to_search in [1, 5]:
-    #for minutes_to_search in [5 * 60]:
+        for minutes_to_search in [1, 5]:
+        #for minutes_to_search in [5 * 60]:
 
-        current_dynamic = []
-        search_time_frozen = minutes_to_search #* 60
-        new_constraint_evaluation_dynamic = ConstraintEvaluation(dataset=test_holdout_dataset_id,
-                                                                 constraint={'search_time': minutes_to_search},
-                                                                 system_def='dynamic')
+            current_dynamic = []
+            search_time_frozen = minutes_to_search #* 60
+            new_constraint_evaluation_dynamic = ConstraintEvaluation(dataset=test_holdout_dataset_id,
+                                                                     constraint={'search_time': minutes_to_search},
+                                                                     system_def='dynamic')
 
-        for repeat in range(10):
+            for repeat in range(10):
 
-            tmp_path = "/home/" + getpass.getuser() + "/data/auto_tmp/autosklearn" + str(time.time()) + '_' + str(np.random.randint(1000)) + 'folder'
+                tmp_path = "/home/" + getpass.getuser() + "/data/auto_tmp/autosklearn" + str(time.time()) + '_' + str(np.random.randint(1000)) + 'folder'
 
-            tracker = EmissionsTracker(save_to_file=False)
-            tracker_inference = EmissionsTracker(save_to_file=False)
-            try:
-                tracker.start()
+                tracker = EmissionsTracker(save_to_file=False)
+                tracker_inference = EmissionsTracker(save_to_file=False)
+                try:
+                    tracker.start()
 
-                pipeline_optimizer = TPOTClassifier(random_state=repeat, max_time_mins=minutes_to_search,
-                                                    scoring='balanced_accuracy', n_jobs=1)
-                pipeline_optimizer.fit(X_train_hold, y_train_hold)
+                    pipeline_optimizer = TPOTClassifier(random_state=repeat, max_time_mins=minutes_to_search,
+                                                        scoring='balanced_accuracy', n_jobs=1)
+                    pipeline_optimizer.fit(X_train_hold, y_train_hold)
 
-                tracker.stop()
+                    tracker.stop()
 
-                tracker_inference.start()
-                y_hat = pipeline_optimizer.predict(X_test_hold)
-                tracker_inference.stop()
-                print("Predictions:  \n", y_hat)
+                    tracker_inference.start()
+                    y_hat = pipeline_optimizer.predict(X_test_hold)
+                    tracker_inference.stop()
+                    print("Predictions:  \n", y_hat)
 
-                result = balanced_accuracy_score(y_test_hold, y_hat)
+                    result = balanced_accuracy_score(y_test_hold, y_hat)
 
-                new_constraint_evaluation_dynamic.append(ConstraintRun('test', 'test', result, more='test', tracker=tracker.final_emissions_data.values, tracker_inference=tracker_inference.final_emissions_data.values, len_pred=len(X_test_hold)))
-            except Exception as e:
-                tracker.stop()
-                tracker_inference.stop()
-                traceback.print_exc()
-                print(e)
-                result = 0
-                new_constraint_evaluation_dynamic.append(ConstraintRun('test', 'shit happened', result, more='test'))
-            finally:
-                if os.path.exists(tmp_path) and os.path.isdir(tmp_path):
-                    shutil.rmtree(tmp_path)
+                    new_constraint_evaluation_dynamic.append(ConstraintRun('test', 'test', result, more='test', tracker=tracker.final_emissions_data.values, tracker_inference=tracker_inference.final_emissions_data.values, len_pred=len(X_test_hold)))
+                except Exception as e:
+                    tracker.stop()
+                    tracker_inference.stop()
+                    traceback.print_exc()
+                    print(e)
+                    result = 0
+                    new_constraint_evaluation_dynamic.append(ConstraintRun('test', 'shit happened', result, more='test'))
+                finally:
+                    if os.path.exists(tmp_path) and os.path.isdir(tmp_path):
+                        shutil.rmtree(tmp_path)
 
-            current_dynamic.append(result)
-            print('dynamic: ' + str(current_dynamic))
-        dynamic_approach.append(current_dynamic)
-        new_constraint_evaluation_dynamic_all.append(new_constraint_evaluation_dynamic)
-        print('dynamic: ' + str(dynamic_approach))
+                current_dynamic.append(result)
+                print('dynamic: ' + str(current_dynamic))
+            dynamic_approach.append(current_dynamic)
+            new_constraint_evaluation_dynamic_all.append(new_constraint_evaluation_dynamic)
+            print('dynamic: ' + str(dynamic_approach))
 
-    results_dict_log = {}
-    results_dict_log['dynamic'] = new_constraint_evaluation_dynamic_all
-    pickle.dump(results_dict_log, open('/home/neutatz/data/automl_runs/log_' + args.outputname + '_' + str(test_holdout_dataset_id) + '.p', 'wb+'))
+        results_dict_log = {}
+        results_dict_log['dynamic'] = new_constraint_evaluation_dynamic_all
+        pickle.dump(results_dict_log, open('/home/neutatz/data/automl_runs/log_' + args.outputname + '_' + str(test_holdout_dataset_id) + '.p', 'wb+'))
 
-    results_dict = {}
-    results_dict['dynamic'] = dynamic_approach
+        results_dict = {}
+        results_dict['dynamic'] = dynamic_approach
     pickle.dump(results_dict, open('/home/neutatz/data/automl_runs/' + args.outputname + '_' + str(test_holdout_dataset_id) + '.p', 'wb+'))
