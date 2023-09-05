6bb636c4afef9ddb8c792bb8391a446a082f76fc
Felix Neutatz <neutatz@googlemail.com>
2023-09-05 18:26:41 +0200

    fix bug

--- fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/opt_full/multiob/opt_it_successive_kill2_newtry40.py
+++ fastsklearnfeature/declarative_automl/optuna_package/myautoml/minimal/week/opt_full/multiob/opt_it_successive_kill2_newtry40.py
@@ -213,7 +213,8 @@ def run_AutoML(task_id, return_dict, dictionary_felix, trial):
                           )
 
         test_score = 0.0
-        tracker_new = EmissionsTracker(project_name='trackautoml', save_to_file=False)
+        tracker_new = EmissionsTracker(project_name='trackautoml' + str(time.time()),
+                                       experiment_id='id_trackautoml' + str(time.time()),  save_to_file=False)
         try:
             tracker_new.start()
             search.fit(X_train, y_train, categorical_indicator=categorical_indicator, scorer=my_scorer)
